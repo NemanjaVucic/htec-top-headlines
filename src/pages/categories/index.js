2729d30ed959154e3ad15bb8aa603243e0671998
Nemanja Vucic <vukaza.nv@gmail.com>
2020-12-09 16:59:54 +0100

    containers added

---
+++ src/pages/categories/index.js
@@ -0,0 +1,5 @@
+const Categories = () => {
+  return <h1>CATEGORIES</h1>;
+};
+
+export default Categories;
---
+++ src/pages/top-news/index.js
@@ -0,0 +1,5 @@
+const TopNews = () => {
+  return <h1>TOP NEWS</h1>;
+};
+
+export default TopNews;
