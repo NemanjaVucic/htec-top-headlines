472c8acc8343feae9c245f2c516c3edb3e81a2ad
Nemanja Vucic <vukaza.nv@gmail.com>
2020-12-13 07:39:01 +0100

    Search component added

--- src/pages/search/index.js
+++ src/pages/search/index.js
@@ -1,5 +1,52 @@
-const Search = () => {
-  return <h1>SEARCH</h1>;
+import { useEffect, useState } from 'react';
+import { connect } from 'react-redux';
+
+import axios from '../../axios';
+import { API_BASE_URL } from '../../shared/constants/api';
+import TopNewsTemplate from '../../shared/container/top-news-template';
+import { selectedCountry } from '../../shared/helpers/';
+
+const Search = ({ language, ...rest }) => {
+  const [searchResults, setSearchResults] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [term, setTerm] = useState('');
+
+  useEffect(async () => {
+    setIsLoading(true);
+    try {
+      const res = await axios.get(
+        `${API_BASE_URL}?country=${language.toLowerCase()}&q=${term}`,
+      );
+      setIsLoading(false);
+      setSearchResults(res.data?.articles);
+    } catch (err) {
+      setIsLoading(false);
+    }
+  }, [language, term]);
+
+  const handleSearchTerm = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setTerm(value);
+  };
+
+  return (
+    <TopNewsTemplate
+      headerText={`Search top news from ${selectedCountry(language)} by term:`}
+      isLoading={isLoading}
+      items={searchResults}
+      term={term}
+      handleSearchTerm={handleSearchTerm}
+      {...rest}
+    />
+  );
 };
 
-export default Search;
+const mapStateToProps = ({ navbar }) => {
+  return {
+    language: navbar.language,
+  };
+};
+
+export default connect(mapStateToProps)(Search);
